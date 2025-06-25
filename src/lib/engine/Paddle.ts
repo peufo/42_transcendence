e4618b6703010c9c3759db6afb06a4f44d7dfc44
Amaury <loubry.amaury@gmail.com>
2025-06-25 22:44:18 +0200

    begin to move engine to backend

---
+++ src/lib/engine/Paddle.ts
@@ -0,0 +1,46 @@
+import { ARENA_HEIGHT, PADDLE_BASE_SPEED, TICK_INTERVAL } from './index.js'
+import type { Move } from './index.js'
+import { Vector2 } from './Vector2.js'
+
+export class Paddle {
+	#position: Vector2
+	#speed = PADDLE_BASE_SPEED
+	#width: number
+	#height: number
+	// acceleration ?
+
+	get position() {
+		return this.#position
+	}
+
+	get width() {
+		return this.#width
+	}
+
+	get height() {
+		return this.#height
+	}
+
+	constructor(position: Vector2, width: number, height: number) {
+		this.#position = new Vector2(position.x, position.y)
+		this.#width = width
+		this.#height = height
+	}
+
+	#move(direction: Move) {
+		const displ = TICK_INTERVAL * this.#speed
+		if (direction === 'up') {
+			if (this.#position.y - displ < 0) this.#position.y = 0
+			else this.#position.y -= displ
+		} else if (direction === 'down') {
+			if (this.#position.y + this.#height + displ > ARENA_HEIGHT)
+				this.#position.y = ARENA_HEIGHT - this.#height
+			else this.#position.y += displ
+		}
+	}
+
+	update(inputs: Record<Move, boolean>) {
+		if (inputs.up) this.#move('up')
+		if (inputs.down) this.#move('down')
+	}
+}
