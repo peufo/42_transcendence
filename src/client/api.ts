b80e637c6bfad44598f19d21253106edf5884072
Loïc Baecher <74916339+loicbaecher@users.noreply.github.com>
2025-07-01 16:11:02 +0200

    add user search

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,20 +1,23 @@
-import { setUser } from './utils/store.js'
+import { setUser, setUsers } from './utils/store.js'
 
 function useApiGetter<Result>(
 	route: string,
 	setter: (value: Result) => void,
-): () => Promise<void> {
-	return async () => {
-		const res = await fetch(route)
+): (query?: string) => Promise<void> {
+	return async (query) => {
+		const url = query ? `${route}?${query}` : route
+		const res = await fetch(url)
 		if (!res.ok) {
 			console.warn('TODO: handle fetch failed... Notifiy ?')
 			return
 		}
 		const json = (await res.json()) as { data: Result }
+		console.log(json)
 		setter(json.data)
 	}
 }
 
 export const api = {
 	user: useApiGetter('/auth/user', setUser),
-}
+	users: useApiGetter('/users', setUsers),
+} as const
