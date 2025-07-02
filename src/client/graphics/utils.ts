3cf84912f692e0e6cfcff961d87892adc91be175
Alex <alex.letondor@outlook.fr>
2025-07-02 14:57:42 +0000

    v1 align with new engine

--- src/client/graphics/utils.ts
+++ src/client/graphics/utils.ts
@@ -1,12 +1,12 @@
 import { ARENA_WIDTH, ARENA_HEIGHT } from "../../lib/engine/index.js";
+
 import { Vector2 } from "../../lib/engine/Vector2.js"
+
 
 export function toRenderPosition(pos: Vector2, width = 0, height = 0): BABYLON.Vector3 {
     return new BABYLON.Vector3(
-        (pos.y + height / 2) - ARENA_HEIGHT / 2, 
+        (pos.y + height / 2 ) - ARENA_HEIGHT / 2, // y devient x babylon
         5,
-        - (pos.x - ARENA_WIDTH / 2)
-
+        - (pos.x + width / 2 - ARENA_WIDTH / 2)  // x devient z babylon
     )
 }
-
