e3144ecd1f09d5bd012a2f637119bd1efd50f854
Alex <alex.letondor@outlook.fr>
2025-07-02 14:57:42 +0000

    v1 align with new engine

--- src/client/graphics/paddles.ts
+++ src/client/graphics/paddles.ts
@@ -1,19 +1,62 @@
 
-export function createPaddles(
-  scene: BABYLON.Scene,
-  material: BABYLON.Material,
-  ARENA_WIDTH: number,
-  ARENA_HEIGHT: number
-): { paddle1: BABYLON.Mesh, paddle2: BABYLON.Mesh } {
+import { PADDLE_BASE_HEIGHT, PADDLE_BASE_WIDTH, PADDLE_BASE_P1_POSITION, PADDLE_BASE_P2_POSITION } from '../../lib/engine/index.js'
+import { toRenderPosition } from './utils.js'
+
+
+export function createPaddles(scene: BABYLON.Scene, material: BABYLON.Material) {
   const paddle1 = BABYLON.MeshBuilder.CreateBox("p1", {
-    width: ARENA_WIDTH / 5,
-    height: ARENA_HEIGHT / 60,   
-    depth: 20
+    width: PADDLE_BASE_HEIGHT,   
+    height: 20,                 
+    depth: PADDLE_BASE_WIDTH,  
   }, scene)
   paddle1.material = material
+  paddle1.position = toRenderPosition(PADDLE_BASE_P1_POSITION)
 
-  const paddle2 = paddle1.clone("p2")!
+  const paddle2 = BABYLON.MeshBuilder.CreateBox("p2", {
+    width: PADDLE_BASE_HEIGHT,
+    height: 20,
+    depth: PADDLE_BASE_WIDTH,
+  }, scene)
   paddle2.material = material
+  paddle2.position = toRenderPosition(PADDLE_BASE_P2_POSITION)
+
   return { paddle1, paddle2 }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/client/graphics/scene.ts
+++ src/client/graphics/scene.ts
@@ -1,14 +1,38 @@
+
+
 import { toRenderPosition } from "./utils.js"
-import { State } from '../../lib/engine/index.js'
+import { PADDLE_BASE_HEIGHT ,PADDLE_BASE_WIDTH,BALL_BASE_SIZE ,State, PADDLE_BASE_P1_POSITION, PADDLE_BASE_P2_POSITION } from '../../lib/engine/index.js'
+import { Vector2 } from "../../lib/engine/Vector2.js"
 
-export function updateGraphics(state: State, ballMesh: any, paddle1: any, paddle2: any): void {
-  const p1 = state.paddles.p1
-  const p2 = state.paddles.p2
-  const ball = state.ball
+export function updateGraphics(
+  state: State,
+  ballMesh: BABYLON.Mesh,
+  paddle1: BABYLON.Mesh,
+  paddle2: BABYLON.Mesh
+): void {
+  ballMesh.position = toRenderPosition(
+    new Vector2(state.b.x, state.b.y),
+    BALL_BASE_SIZE,
+    BALL_BASE_SIZE
+  );
 
-  ballMesh.position = toRenderPosition(ball.position, ball.size, ball.size)
-  paddle1.position = toRenderPosition(p1.position, p1.width, p1.height)
-  paddle2.position = toRenderPosition(p2.position, p2.width, p2.height)
+  const p1Pos = new Vector2(
+    PADDLE_BASE_P1_POSITION.x,
+    state.p1
+  );
+  paddle1.position = toRenderPosition(
+    p1Pos,
+    PADDLE_BASE_WIDTH,
+    PADDLE_BASE_HEIGHT
+  );
 
+  const p2Pos = new Vector2(
+    PADDLE_BASE_P2_POSITION.x,
+    state.p2
+  );
+  paddle2.position = toRenderPosition(
+    p2Pos,
+    PADDLE_BASE_WIDTH,
+    PADDLE_BASE_HEIGHT
+  );
 }
-
