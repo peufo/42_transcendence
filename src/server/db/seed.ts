72aef8f1cf2ef3d2160ca3c5618b1cf5f3c92762
jonas <jonas.voisard@gmail.com>
2025-07-03 13:31:15 +0200

    looking for new friends research

--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/libsql'
 import { seed } from 'drizzle-seed'
-import { users } from './index.js'
+import { users, friendships } from './schema.js'
 import { env } from '../env.js'
 import argon2 from 'argon2'
 import { createAvatarPlaceholder } from '../controllers/avatar.js'
@@ -15,13 +15,31 @@ async function main() {
 				avatar: f.default({
 					defaultValue: '',
 				}),
-				avatarPlaceholder: f.default({
-					defaultValue: createAvatarPlaceholder(),
+				avatarPlaceholder: f.valuesFromArray({
+					values: new Array(10).fill('').map(createAvatarPlaceholder),
 				}),
 				passwordHash: f.default({
 					defaultValue: passwordHash,
 				}),
 			},
+			count: 10,
+		},
+	}))
+
+	await seed(db, { friendships }).refine((f) => ({
+		friendships: {
+			columns: {
+				user1Id: f.valuesFromArray({
+					values: [1, 2, 3],
+				}),
+				user2Id: f.valuesFromArray({
+					values: [4, 5, 6, 7, 8, 9, 10],
+				}),
+				state: f.valuesFromArray({
+					values: ['invited', 'friend'],
+				}),
+			},
+			count: 12,
 		},
 	}))
 }
