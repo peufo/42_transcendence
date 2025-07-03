8d5b28e91c54db1776ed2d8238da606da80ebe95
jonas <jonas.voisard@gmail.com>
2025-07-03 17:58:38 +0200

    Add friend ok

--- src/server/routes/users.ts
+++ src/server/routes/users.ts
@@ -6,7 +6,7 @@ import { getTableColumns } from 'drizzle-orm'
 import '@fastify/cookie'
 import type { Friendship } from '../types.js'
 
-const auth: FastifyPluginCallbackZod = (server, options, done) => {
+export const usersRoute: FastifyPluginCallbackZod = (server, options, done) => {
 	server.get(
 		'/',
 		{
@@ -57,46 +57,7 @@ const auth: FastifyPluginCallbackZod = (server, options, done) => {
 		return res.send({ data: friends })
 	})
 
-	server.get('/invitations', async (req, res) => {
-		const user = res.locals?.user
-		if (!user) return res.code(401).send()
-		const invitations = await getInvitations(user.id)
-		// TODO: add gameId
-		return res.send({ data: invitations })
-	})
 	done()
-}
-
-async function getInvitations(userId: number) {
-	const res = await db.query.friendships.findMany({
-		where: and(
-			or(eq(friendships.user1Id, userId), eq(friendships.user2Id, userId)),
-			eq(friendships.state, 'invited'),
-		),
-		with: {
-			user1: {
-				columns: {
-					passwordHash: false,
-					createdAt: false,
-					lastLogin: false,
-					isActive: false,
-				},
-			},
-			user2: {
-				columns: {
-					passwordHash: false,
-					createdAt: false,
-					lastLogin: false,
-					isActive: false,
-				},
-			},
-		},
-	})
-	return res.map((r) => {
-		if (r.user1Id === userId)
-			return { createdBy: r.createdBy, createdAt: r.createdAt, ...r.user2 }
-		return { createdBy: r.createdBy, createdAt: r.createdAt, ...r.user1 }
-	})
 }
 
 async function getUserFriendships(userId: number, state?: Friendship['state']) {
@@ -118,5 +79,3 @@ async function getUserFriendsId(userId: number, state?: Friendship['state']) {
 		friendship.user1Id === userId ? friendship.user2Id : friendship.user1Id,
 	)
 }
-
-export default auth
