998a3c9d299a7ca747e2fe49a17c95552dc3fccf
Alex <alex.letondor@outlook.fr>
2025-07-06 20:00:52 +0000

    starting working on signup in front and back. need to add avatar handling in the front.

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -7,7 +7,7 @@ import { createSession } from '../controllers/session.js'
 import { db, users } from '../db/index.js'
 import '@fastify/cookie'
 import '../types.js'
-
+import { signupUser } from '../controllers/user.js'
 export const authRoute: FastifyPluginCallbackZod = (server, options, done) => {
 	server.post(
 		'/login',
@@ -49,6 +49,22 @@ export const authRoute: FastifyPluginCallbackZod = (server, options, done) => {
 			res.redirect('/')
 		},
 	)
+
+	// signup
+	server.post(
+		'/signup',
+		{
+			schema: {
+				body: z.object({
+					username: z.string(),
+					password: z.string(),
+					avatar: z.string(),
+				}),
+			},
+		},
+		signupUser,
+	)
+
 	server.post('/logout', async (req, res) => {
 		const now = new Date()
 		res.setCookie('session', '', {
