0066a9b3a9612a76722b32663ec24904174e2e3e
Alex <alex.letondor@outlook.fr>
2025-07-07 19:34:09 +0000

    skybox

--- src/client/graphics/materials.ts
+++ src/client/graphics/materials.ts
@@ -8,9 +8,9 @@ export function createPaddleMaterial(
 	return mat
 }
 
-export function createBallMaterial(scene: BABYLON.Scene): BABYLON.PBRMaterial {
-	const ballMat = new BABYLON.PBRMaterial('ballMat', scene)
-	ballMat.albedoColor = new BABYLON.Color3(1, 1, 1)
+export function createBallMaterial(scene: BABYLON.Scene): BABYLON.StandardMaterial {
+	const ballMat = new BABYLON.StandardMaterial('ballMat', scene)
+	ballMat.diffuseColor = new BABYLON.Color3(1, 1, 1)
 	ballMat.metallic = 0.5
 	ballMat.roughness = 0.2
 	ballMat.disableLighting = false
@@ -19,17 +19,17 @@ export function createBallMaterial(scene: BABYLON.Scene): BABYLON.PBRMaterial {
 
 export function createPaddleMaterial2(
 	scene: BABYLON.Scene,
-): BABYLON.PBRMaterial {
-	const mat = new BABYLON.PBRMaterial('newPaddlemat', scene)
-	mat.albedoColor = new BABYLON.Color3(1, 1, 1)
+): BABYLON.StandardMaterial {
+	const mat = new BABYLON.StandardMaterial('newPaddlemat', scene)
+	mat.diffuseColor = new BABYLON.Color3(1, 1, 1)
 	mat.metallic = 1
 	mat.roughness = 1
 	return mat
 }
 
-export function createMatwall(scene: BABYLON.Scene): BABYLON.PBRMaterial {
-	const arenaMat = new BABYLON.PBRMaterial('arenaMat', scene)
-	arenaMat.albedoColor = new BABYLON.Color3(0.1, 0.1, 0.1) // couleur sombre
+export function createMatwall(scene: BABYLON.Scene): BABYLON.StandardMaterial {
+	const arenaMat = new BABYLON.StandardMaterial('arenaMat', scene)
+	arenaMat.diffuseColor = new BABYLON.Color3(0.1, 0.1, 0.1) // couleur sombre
 	arenaMat.emissiveColor = new BABYLON.Color3(0.03, 0.03, 0.03) // effet lumineux
 	arenaMat.metallic = 1
 	arenaMat.roughness = 1
