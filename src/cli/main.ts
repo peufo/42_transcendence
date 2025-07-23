91e1f7b7728eaf314d31bf402ac5ae3af17b69a1
jonas <jonas.voisard@gmail.com>
2025-07-24 00:46:04 +0200

    cli: save session token in file

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,11 +1,12 @@
 import * as p from '@clack/prompts'
-
+import { loadApiOptions } from './api.js'
 import { menuMain } from './menuMain.js'
 
 export type Scope = () => Promise<Scope | null> | (Scope | null)
 export type ScopeOptions = p.SelectOptions<Scope | null>['options']
 
 p.intro('Welcome to transcendance')
+await loadApiOptions()
 
 let scope: Scope | null = menuMain
 while (scope) {
