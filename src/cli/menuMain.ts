dc10700e89a856fc39ddce69d9eae567e2da6edf
jonas <jonas.voisard@gmail.com>
2025-07-23 19:52:50 +0200

    cli: create tournament

--- src/cli/menuMain.ts
+++ src/cli/menuMain.ts
@@ -5,18 +5,20 @@ import { login, logout } from './auth.js'
 import { start } from './game.js'
 import type { Scope, ScopeOptions } from './main.js'
 import { menuFriendships } from './menuFriendships.js'
+import { menuNewTournament } from './menuTournament.js'
 
 export const menuMain: Scope = async () => {
 	const options: ScopeOptions = []
 
 	if (!api.user()) {
-		options.push({ value: login, label: 'Login' })
+		options.push({ label: 'Login', value: login })
 	} else {
-		options.push({ value: menuFriendships, label: 'Friendships' })
-		options.push({ value: logout, label: 'Logout' })
+		options.push({ label: 'Logout', value: logout })
+		options.push({ label: 'Friendships', value: menuFriendships })
+		options.push({ label: 'New tournament', value: menuNewTournament })
 	}
 	options.push(
-		{ value: start, label: 'Start game' },
+		{ value: start, label: 'Local game' },
 		{ value: exit, label: 'Exit' },
 	)
 	const action = await p.select({ message: 'What you want to do ?', options })
