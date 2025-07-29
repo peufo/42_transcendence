c84f6449d9cc428d861a6cad37d24a89f4ec9f5b
jonas <jonas.voisard@gmail.com>
2025-07-29 20:16:56 +0200

    Add userIsBusy() check before create or join tournament

--- src/server/routes/tournaments/model.ts
+++ src/server/routes/tournaments/model.ts
@@ -1,11 +1,26 @@
-import { and, eq } from 'drizzle-orm'
+import { and, eq, ne } from 'drizzle-orm'
 import type { UserBasic } from '../../../lib/type.js'
 import { db, tournaments, tournamentsParticipants } from '../../db/index.js'
 import { server } from '../../main.js'
 import type { DB } from '../../types.js'
 import { getUserBasic, userBasicColumns } from '../friendships/model.js'
 
+async function getUserBusy(userId: number): Promise<boolean> {
+	const participations = db
+		.select()
+		.from(tournamentsParticipants)
+		.where(eq(tournamentsParticipants.userId, userId))
+		.as('participations')
+	const tournamentOpenOrOngoing = await db
+		.select()
+		.from(tournaments)
+		.innerJoin(participations, ne(tournaments.state, 'finished'))
+	return !!tournamentOpenOrOngoing.length
+}
+
 export async function tournamentCreate(data: DB.TournamentCreate) {
+	const userIsBusy = await getUserBusy(data.createdBy)
+	if (userIsBusy) throw server.httpErrors.forbidden('Sorry, your busy')
 	const tournamentId = await db.transaction(async (tx) => {
 		const [{ id, createdBy }] = await tx
 			.insert(tournaments)
@@ -49,6 +64,8 @@ export async function tournamentJoin(
 	tournamentId: number,
 	userId: number,
 ): Promise<UserBasic> {
+	const userIsBusy = await getUserBusy(userId)
+	if (userIsBusy) throw server.httpErrors.forbidden('Sorry, your busy')
 	const tournament = await tournamentGet(tournamentId)
 	if (tournament.participants.length >= tournament.numberOfPlayers)
 		throw server.httpErrors.forbidden(
